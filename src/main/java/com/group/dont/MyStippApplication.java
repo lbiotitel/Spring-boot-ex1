fc289b1ddb6d71f0e0327c04abaa24475c54d0be
biotite <biotite135@naver.com>
2021-10-04 22:32:53 +0900

    2021-10-04

---
+++ src/main/java/com/group/dont/MyStippApplication.java
@@ -0,0 +1,13 @@
+package com.group.dont;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyStippApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyStippApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group/dont/MyStippApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.dont;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyStippApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
